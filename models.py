704fe324a75bc0c1903ab4e547423f65aa35a1bf
Diego RGZ <lalorgz216@gmail.com>
2025-12-14 00:30:53 -0600

    Define ORM models and fix relationships

--- models.py
+++ models.py
@@ -4,20 +4,16 @@ db = SQLAlchemy()
 
 class Cliente(db.Model):
     __tablename__ = "cliente"
-
     id_cliente = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String)
-    apellido = db.Column(db.String)
 
 class Empleado(db.Model):
     __tablename__ = "empleado"
-
     id_empleado = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String)
 
 class Producto(db.Model):
     __tablename__ = "producto"
-
     id_producto = db.Column(db.Integer, primary_key=True)
     titulo = db.Column(db.String)
 
@@ -25,16 +21,16 @@ class Renta(db.Model):
     __tablename__ = "renta"
 
     id_renta = db.Column(db.Integer, primary_key=True)
-    id_cliente = db.Column(db.Integer)
-    id_empleado = db.Column(db.Integer)
-    id_producto = db.Column(db.Integer)
-
     fecha_renta = db.Column(db.Date)
     fecha_devolucion = db.Column(db.Date)
-
     estado_renta = db.Column(db.String)
-    pago_total = db.Column(db.Float)
+    pago_total = db.Column(db.Numeric)
 
-  #  cliente = db.relationship("Cliente")
-  # empleado = db.relationship("Empleado")
-  #  producto = db.relationship("Producto")
+    id_cliente = db.Column(db.Integer, db.ForeignKey("cliente.id_cliente"))
+    id_empleado = db.Column(db.Integer, db.ForeignKey("empleado.id_empleado"))
+    id_producto = db.Column(db.Integer, db.ForeignKey("producto.id_producto"))
+
+    cliente = db.relationship("Cliente")
+    empleado = db.relationship("Empleado")
+    producto = db.relationship("Producto")
+
